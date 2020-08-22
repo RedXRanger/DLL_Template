64a5f29cd6b8547a42a85a90b124d27e746ca9ea
RedXRanger <53659315+RedXRanger@users.noreply.github.com>
2020-08-22 12:55:27 -0400

    Update CreateThread_DLL.c

--- CreateThread_DLL.c
+++ CreateThread_DLL.c
@@ -1,5 +1,7 @@
 //This is completely based off samprograms dllpost linked below
 //Its a simple DLL template that can be used for process injection, or whatever you want to happen within the Myfunction()
+//Uses createThread winAPI function to do its business
+//The thread attach case basically means execute once this dll is run. (so it'll run MyFunction when you run it with rundll32.exe)
 //https://samprograms.wordpress.com/2015/02/27/correctly-using-createthread/
 
 void MyFunction()
@@ -16,6 +18,9 @@ BOOL APIENTRY DllMain(
     {
     case DLL_PROCESS_ATTACH:
     CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MyFunction, 0, 0, 0);
+    //DLL_PROCESS_ATTACH — Indicates that the DLL is being loaded into -
+    //the virtual address space of the current process as a result of a call to LoadLibrary. 
+    //DLL_PROCESS_ATTACH is only called for the first process that attaches to it.
     case DLL_PROCESS_DETACH:
     break;
     case DLL_THREAD_ATTACH:
